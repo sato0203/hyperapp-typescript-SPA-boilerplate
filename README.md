15b4095fb831757cc34a0eaaea6aae33a83f2770
佐藤輝年 <sato.geijo.13@gmail.com>
2019-01-30 12:25:42 +0900

    一旦SSR、Helmetの処理を除外してシンプルに

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# hyperapp-typescript-sample
-Hyperapp Sample Project with TypeScript.
+
+
+# 要修正(VSCode対応)
+
+Routeの型定義を
+`VNode<RenderProps<P>> | void`から`VNode<RenderProps<P>> | null`に変えないと
+VSCodeに怒られます。
+
+```
+export function Route<P>(
+  props: RouteProps<P>
+): VNode<RenderProps<P>> | null;
+```
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,10 @@ import { location } from '@hyperapp/router'
 
 import { App } from './app'
 
-const container = document.getElementById('app')
-let main
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  main = require('@hyperapp/logger').withLogger(app)(
-    App.state,
-    App.actions,
-    App.view,
-    container
-  )
-} else {
-  main = app(App.state, App.actions, App.view, container)
-}
+const main = app(
+    App.state, 
+    App.actions, 
+    App.view, 
+    document.getElementById('app')
+)
 const unsubscribe = location.subscribe(main.location)
